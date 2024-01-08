b7fe93c8e7d61e634b92a5d88a09394b7a05d1f0
Konnor <konnor.frik666@gmail.com>
2024-01-08 16:39:50 +0500

    develop: sand moving through water

--- field/rules.c
+++ field/rules.c
@@ -16,25 +16,36 @@ enum {
 #include <stdio.h>
 #endif
 
+static int can_sand_move(int type) {
+    return type == NONE || type == WATER;
+}
+
 void sand_rule(Field* field, size_t curr_row, size_t curr_col) {
     if (!(curr_row + 1 < field->rows)) {
         return;
     }
 
-    if (field->field[curr_row + 1][curr_col]->type == NONE) { // if 1 down is empty
+    if (can_sand_move(field->field[curr_row + 1][curr_col]->type)) { // if 1 down is empty
         cell_swap(&(field->field[curr_row + 1][curr_col]),
                   &(field->field[curr_row][curr_col]));
 
-    } else { // random left right move
-        if (rand() % 2 == 0 && curr_col > 0 && (field->field[curr_row + 1][curr_col - 1]->type == NONE)) { // move left
+    } else if (curr_col > 0 && curr_col < field->columns - 1) { // random left right move
+        if (rand() % 2 == LEFT && curr_col > 0 && can_sand_move(field->field[curr_row + 1][curr_col - 1]->type)) { // move left
             cell_swap(&(field->field[curr_row][curr_col]),
                     &(field->field[curr_row + 1][curr_col - 1]));
 
-        } else if (curr_col < field->columns - 1 && field->field[curr_row + 1][curr_col + 1]->type == NONE) { // move right
+        } else if (curr_col < field->columns - 1 && can_sand_move(field->field[curr_row + 1][curr_col + 1]->type)) { // move right
             cell_swap(&(field->field[curr_row][curr_col]),
                     &(field->field[curr_row + 1][curr_col + 1]));
         }
 
+    } else if ((field->columns - curr_col) == 1 && can_sand_move(field->field[curr_row + 1][curr_col - 1]->type)) { // right border
+        cell_swap(&(field->field[curr_row][curr_col]),
+                &(field->field[curr_row + 1][curr_col - 1]));
+
+    } else if (curr_col == 0 && can_sand_move(field->field[curr_row + 1][curr_col + 1]->type)) { // left border
+        cell_swap(&(field->field[curr_row][curr_col]),
+                &(field->field[curr_row + 1][curr_col + 1]));
     }
 }
 
